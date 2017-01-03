b8d2c855f1407e7ef5da115f3b9027de9424f782
Apurv <apurv135068@nitp.ac.in>
2017-01-03 12:36:04 +0530

    Proceed to Publish

---
+++ app/src/main/java/com/example/apurv/navriti/StreamingActivity.java
@@ -0,0 +1,13 @@
+package com.example.apurv.navriti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StreamingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_streaming);
+    }
+}
